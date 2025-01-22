05b1a7bbc5809f30d999efbb3ca0fe88213df599
LeviMaycon <levioficial361@gmail.com>
2025-01-22 01:23:23 -0300

    style: change epsodies style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,11 @@ import { EpsodiesComponent } from "./components/epsodies/epsodies.component";
 import { CharactersComponent } from "./components/characters/characters.component";
 import { VideosComponent } from "./components/videos/videos.component";
 import { ArtsComponent } from "./components/arts/arts.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, AboutComponent, EpsodiesComponent, CharactersComponent, VideosComponent, ArtsComponent],
+  imports: [RouterOutlet, HeaderComponent, AboutComponent, EpsodiesComponent, CharactersComponent, VideosComponent, ArtsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
