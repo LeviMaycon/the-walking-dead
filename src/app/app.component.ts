0b1ebcecdb1e4c9e6b1532688e6f1e8a035efde9
LeviMaycon <levioficial361@gmail.com>
2025-01-22 00:40:53 -0300

    feat: create characters component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { AboutComponent } from "./components/about/about.component";
 import { EpsodiesComponent } from "./components/epsodies/epsodies.component";
+import { CharactersComponent } from "./components/characters/characters.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, AboutComponent, EpsodiesComponent],
+  imports: [RouterOutlet, HeaderComponent, AboutComponent, EpsodiesComponent, CharactersComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
